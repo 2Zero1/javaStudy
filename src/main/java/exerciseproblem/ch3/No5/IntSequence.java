12367846271f2bb80d82c3a0ec603cb4b63e4bc2
2Zero1 <Good7219@hotmail.com>
2020-03-10 19:24:04 +0900

    Failed to solved practice No6 because cannot understand the purpose of practice.

---
+++ src/main/java/exerciseproblem/ch3/No5/IntSequence.java
@@ -0,0 +1,18 @@
+package exerciseproblem.ch3.No5;
+
+import exerciseproblem.ch3.No4.Sequence;
+
+import java.util.ArrayList;
+
+/*익명을 사용하지 않음
+무한 상수 시퀀스를 돌려주는 IntSequence 클래스의 정적 메서드 constant를 추가하라.(예를 들어 IntSequence.constant(1)은
+ 1 1 1... 을 돌려준다. 람다 표현식으로 만들면 추가 점수가 있다.)
+ 문제가 이해가 되지 않는다.
+ */
+class IntSequence {
+
+    public static void constant(int a) {
+
+    }
+
+}
