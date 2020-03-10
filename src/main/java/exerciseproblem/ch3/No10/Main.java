cd3a96a4a6657d498b73d248be8162a41fe33cca
2Zero1 <Good7219@hotmail.com>
2020-03-10 21:08:20 +0900

    Solve No10 Practice
    다음 메서드를 구현하라.
    public static void runTogether(Runnable... tasks)
    public static void runInOrder(Runnable... tasks)
    첫 번째 메서드는 각 클래스를 별도의 스레드에서 실행한 후 반환해야 한다. 두 번째 메서드는 모든 태스크를 현재 스레드에서 실행하고 마지막  태스크가 완료 될 때 반환해야 한다.
    풀이
    - runnable을 ArrayList<Runnable>에 담은 후에, 메서드에 넣어줄 배열 형태를 만들어 줌. 제네릭타입으로 구성된 배열을 자바에선 생성할 수 없기 때문에 ArrayList를 stream으로 만들어 준 후에 toArray에 생성자 람다를 넣어 해결 하였음

--- src/main/java/exerciseproblem/ch3/No10/Main.java
+++ src/main/java/exerciseproblem/ch3/No10/Main.java
@@ -1,4 +1,34 @@
 package exerciseproblem.ch3.No10;
 
+import java.util.ArrayList;
+
+/*
+다음 메서드를 구현하라.
+public static void runTogether(Runnable... tasks)
+public static void runInOrder(Runnable... tasks)
+첫 번째 메서드는 각 클래스를 별도의 스레드에서 실행한 후 반환해야 한다. 두 번째 메서드는 모든 태스크를 현재 스레드에서 실행하고 마지막의
+태스크가 완료 될 때 반환해야 한다.
+ */
 public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Runnable> arr = new ArrayList<>();
+        for (int i = 0 ; i < 10; i ++) {
+            arr.add(new Task(i,i+"테스크"));
+        }
+        runTogether(arr.stream().toArray(Runnable[] :: new));
+        runInOrder(arr.stream().toArray(Runnable[] :: new));
+
+    }
+
+    public static void runTogether(Runnable... tasks) {
+        for (int i = 0; i < tasks.length; i++) {
+            new Thread(tasks[i]).start();
+        }
+    }
+    public static void runInOrder(Runnable... tasks) {
+        for (int i = 0; i < tasks.length; i++) {
+            tasks[i].run();
+        }
+    }
 }
--- src/main/java/exerciseproblem/ch3/No10/Task.java
+++ src/main/java/exerciseproblem/ch3/No10/Task.java
@@ -1,4 +1,18 @@
 package exerciseproblem.ch3.No10;
 
-public class Task {
+public class Task implements Runnable {
+    int n;
+    String name;
+
+    public Task(int n, String name) {
+        this.n = n;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0 ; i < n; i++) {
+            System.out.println(this.name);
+        }
+    }
 }
