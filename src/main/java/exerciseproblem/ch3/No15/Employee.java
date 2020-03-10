baca4156bd3fa93c22b470f3c5dee79f29a46602
2Zero1 <Good7219@hotmail.com>
2020-03-10 23:02:20 +0900

    Solve No15 practice
    문제 : 급여에 따라 직원을 정렬하는 Arrays.sort 호출을 작성하라. 순위가 같은 직원은 이름으로 정렬하라. 이때는 Comparator.thenComparing
    을 사용하면 된다. 그런 다음 이 작업을 역순으로 수행하라.

--- src/main/java/exerciseproblem/ch3/No15/Employee.java
+++ src/main/java/exerciseproblem/ch3/No15/Employee.java
@@ -1,4 +1,20 @@
 package exerciseproblem.ch3.No15;
 
 public class Employee {
+
+    String name;
+    double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
 }
--- src/main/java/exerciseproblem/ch3/No15/Main.java
+++ src/main/java/exerciseproblem/ch3/No15/Main.java
@@ -1,4 +1,24 @@
-package exerciseproblem.ch3;
+package exerciseproblem.ch3.No15;
 
-public class No15 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/*
+급여에 따라 직원을 정렬하는 Arrays.sort 호출을 작성하라. 순위가 같은 직원은 이름으로 정렬하라. 이때는 Comparator.thenComparing
+을 사용하면 된다. 그런 다음 이 작업을 역순으로 수행하라.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Employee> ar = new ArrayList<>();
+        ar.add(new Employee("grace",1000));
+        ar.add(new Employee("peggy",1000));
+        ar.add(new Employee("han",1000));
+        ar.add(new Employee("jack",10000));
+        Employee[] employees = ar.stream().toArray(Employee[] ::new);
+        Arrays.sort(employees, Comparator.comparing(Employee::getSalary, (x,y) -> Double.compare(y,x)).thenComparing(Employee::getName,Comparator.reverseOrder()));
+        ArrayList<Employee> arrayList = new ArrayList<>(List.of(employees));
+        arrayList.forEach((v) -> System.out.println(v.getName()));
+    }
 }
