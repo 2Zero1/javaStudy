1c622bce0a2a24fe67d634640a92bf36fd445978
2Zero1 <Good7219@hotmail.com>
2020-03-10 18:20:02 +0900

    Solve No3 Practice
    String, Scanner, ImageOutputStream의 슈퍼 타입을 모두 나열하라.( 각 타입은 그 자신의 슈퍼타입이라는 점을 유의한다. 슈퍼 타입을
        선언하지 않은 클래스나 인터페이스는 Object를 슈퍼타입으로 둔다.)

---
+++ src/main/java/exerciseproblem/ch3/No3/Main.java
@@ -0,0 +1,14 @@
+package exerciseproblem.ch3.No3;
+
+import javax.imageio.stream.ImageOutputStream;
+import java.util.Scanner;
+
+public class Main {
+    //String, Scanner, ImageOutputStream의 슈퍼 타입을 모두 나열하라.( 각 타입은 그 자신의 슈퍼타입이라는 점을 유의한다. 슈퍼 타입을
+    //선언하지 않은 클래스나 인터페이스는 Object를 슈퍼타입으로 둔다.)
+    public static void main(String[] args) {
+        //String java.io.Serializable, Comparable<String>, CharSequence, String
+        //Scanner  Iterator<String>, Closeable, Scanner
+        //ImageOutputStream ImageInputStream, DataOutput, ImageOutputStream
+    }
+}
