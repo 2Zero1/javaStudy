1b17023ceb1a23fb5f2060bc2157606ce121784f
2Zero1 <Good7219@hotmail.com>
2020-03-10 19:09:53 +0900

    Update IntSequence

--- src/main/java/exerciseproblem/ch3/No4/IntSequence.java
+++ src/main/java/exerciseproblem/ch3/No4/IntSequence.java
@@ -5,20 +5,26 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
+/*익명을 사용하지 않음
+- array를 stream으로 만들어 맵으로 하나씩 sequence로 변경.
+- 배열을 ArrayList<Sequence>로 만든 후 Sequence[] 형태로 변환하였음.
+ */
 interface IntSequence {
     static IntSequence[] of(int... ints) {
-        List<Integer> names = List.of(1,2,3,4);
-        Stream<Sequence> stream = names.stream().map(Sequence::new);
-        
-        
-        return stream.toArray(Sequence[]::new);
-        
-        //        ArrayList<Integer> arr = new ArrayList<>();
-//
-//        for(int i = 0; i < ints.length; i ++) {
-//            arr.add(Integer.valueOf(ints[i]));
+//        Integer[] arr = new Integer[ints.length];
+//        for (int i = 0; i < arr.length; i ++) {
+//            arr[i] = Integer.valueOf(ints[i]);
 //        }
-//        Stream<Integer> arr2 = arr.stream();
-//        return arr2.toArray(Sequence[] :: new);
+//        List<Integer> names = List.of(arr);
+//        Stream<Sequence> stream = names.stream().map(Sequence::new);
+//
+//
+//        return stream.toArray(Sequence[]::new);
+
+        ArrayList<Sequence> arr = new ArrayList<>();
+        for(int item : ints) {
+            arr.add(new Sequence(item));
+        }
+        return arr.stream().toArray(Sequence[] :: new);
     }
 }
--- src/main/java/exerciseproblem/ch3/No4/IntSequence2.java
+++ src/main/java/exerciseproblem/ch3/No4/IntSequence2.java
@@ -1,5 +1,7 @@
 package exerciseproblem.ch3.No4;
 
+
+//익명 인스턴스를 이용하여 푼 풀이
 public class IntSequence2 {
     public static IntSequenceInterface[] of (int... ints) {
        IntSequenceInterface[] arr = new IntSequenceInterface[ints.length];
